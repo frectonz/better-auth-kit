1111f1662a2ad621bc127a47c0b3e497e466e3c0
frectonz <fraol0912@gmail.com>
2025-08-14 18:44:45 +0300

    chore: format files

--- packages/plugins/app-invite/build.ts
+++ packages/plugins/app-invite/build.ts
@@ -2,6 +2,6 @@ import { build, type Config } from "@better-auth-kit/internal-build";
 
 export const config: Config = {
 	enableDts: true,
-	entrypoints: ['./src/index.ts', './src/client.ts'],
+	entrypoints: ["./src/index.ts", "./src/client.ts"],
 };
 build(config);
